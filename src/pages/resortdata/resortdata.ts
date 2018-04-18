d6ec5503bb91ac913f6a038b1c36f5378aa749e3
Suraj0094 <38438405+Suraj0094@users.noreply.github.com>
2018-04-18 18:15:16 +0530

    Add files via upload

--- src/pages/resortdata/resortdata.ts
+++ src/pages/resortdata/resortdata.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 
 /**
  * Generated class for the ResortdataPage page.
@@ -17,16 +18,18 @@ import { RequestOptions } from '@angular/http';
 })
 export class ResortdataPage {
   token:string;
-
+  data:Observable<any>;
   constructor(public navCtrl: NavController, public navParams: NavParams,public http:HttpClient) {
     console.log("In ResortData controller")
     this.token = 'OlHPqkMUaTVlioS3xXhYDdYVX73MGVo_x7j2zUUvjstTKt09dCjaXjLRyH6z1-NrZE2gXH5agXMcwwMY_tGEC2g5brR_12cnJ4x17Eb7g8edduGIuidFASevqcHPWnYx';
-    const headers = new HttpHeaders().set('Authorization', this.token);
+    const headers = new HttpHeaders().set('Authorization',this.token);
     this.http.get('https://api.yelp.com/v3/businesses/search?latitude=37.786882&longitude=-122.399972', { headers }).subscribe(data =>{
-      console.log(data);
+        console.log(data);
     });
-
-  }
+}
+  
+    
+  
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ResortdataPage');
