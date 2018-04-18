759c45ce78916d68449062de31b1859e1cc2879d
Prashant Sable <prashant7090@gmail.com>
2018-04-18 15:46:48 +0530

    Possible fixed for Headers

--- src/pages/resortdata/resortdata.ts
+++ src/pages/resortdata/resortdata.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { HttpClient } from '@angular/common/http';
-import {Http, RequestOptions} from '@angular/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { RequestOptions } from '@angular/http';
+
 /**
  * Generated class for the ResortdataPage page.
  *
@@ -18,20 +19,12 @@ export class ResortdataPage {
   token:string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,public http:HttpClient) {
-    
-    let headers = new Headers();
+    console.log("In ResortData controller")
     this.token = 'OlHPqkMUaTVlioS3xXhYDdYVX73MGVo_x7j2zUUvjstTKt09dCjaXjLRyH6z1-NrZE2gXH5agXMcwwMY_tGEC2g5brR_12cnJ4x17Eb7g8edduGIuidFASevqcHPWnYx';
-    headers.append('Authorization', 'Bearer ' + this.token);
-    headers.append('Content-Type', 'application/x-www-form-urlencoded');
-    headers.append('Accept', 'application/json');
-    let options = new RequestOptions({ headers: headers });
-
-    console.log('hello data provider');
-    this.http.get('https://api.yelp.com/v3/businesses/search?latitude=37.786882&longitude=-122.399972',options).subscribe(data =>{
+    const headers = new HttpHeaders().set('Authorization', this.token);
+    this.http.get('https://api.yelp.com/v3/businesses/search?latitude=37.786882&longitude=-122.399972', { headers }).subscribe(data =>{
       console.log(data);
-  
     });
-
 
   }
 
